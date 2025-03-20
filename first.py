ec0e0787fc2c5dc595ba52ecb17d794a7616530d
Sakshi <sakshisinghrajput678@gmail.com>
2025-03-20 15:58:39 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,47 @@
+# Write a Python program to check if a number is positive, negative, or zero using if-else statements.
+# Create a Python function that takes a list of numbers and returns the largest number using a for loop.
+# Write a Python program to print the first 10 natural numbers using a while loop.
+# Develop a Python script to calculate the factorial of a number using a for loop.
+# Write a Python program to find the sum of all even numbers between 1 and 100 using a for loop.
+# Create a Python function that takes a list of numbers and prints only the odd numbers using a for loop and continue statement.
+
+
+# def check(num):
+#     if num>0:
+#         print ("number is positive")
+#     elif num<0:
+#         print ("number is negative")
+#     else :
+#         print("number is zero")
+# check(9)
+
+# def largest_number(numbers):
+#     if not numbers:
+#         return None
+
+#     largest = numbers[0]
+
+#     for num in numbers:
+#         if num > largest:
+#             largest = num
+#     return largest
+
+# numbers_list =[3,7,2,9,5,10,1]
+# print(largest_number(numbers_list))
+
+
+num =1
+while num<=10:
+    print (num)
+    num+=1
+
+
+
+
+
+
+
+
+
+
+
